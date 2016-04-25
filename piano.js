c8bc5b079f1600feb0c5df9d0c66dcc2f4909556
Gabriel Reimers <gabriel@mr-fridge.de>
2016-04-25 17:54:08 +0200

    - made keyboard be generated by JavaScript with configurable number of octaves

--- piano.js
+++ piano.js
@@ -9,11 +9,11 @@ var KEYS_PER_OCTAVE = 17;
 var verovioToolkit = new verovio.toolkit();
 
 function paeCodeForKeyAtIndex(keyIndex, baseOctave, duration) {
-    var octaveOffset = Math.floor(keyIndex / KEYS_PER_OCTAVE);
-    var octaveIndex = baseOctave + octaveOffset;
-    var octaveSigns = [",,,", ",,", ",", "'", "''", "'''", "''''"];
-    var octaveSign = octaveSigns[octaveIndex];
-    console.log("octaveInd = " + baseOctave + " + " + octaveOffset);
+    var octaveOffset = Math.floor(keyIndex / KEYS_PER_OCTAVE)
+    var octaveIndex = baseOctave + octaveOffset
+    var octaveSigns = [",,,", ",,", ",", "'", "''", "'''", "''''"]
+    var octaveSign = octaveSigns[octaveIndex]
+    console.log("octaveInd = " + baseOctave + " + " + octaveOffset)
     var notes = [duration + "C",
         "x" + duration + "C",
         "b" + duration + "D",
@@ -31,26 +31,26 @@ function paeCodeForKeyAtIndex(keyIndex, baseOctave, duration) {
         duration + "A",
         "x" + duration + "A",
         "b" + duration + "B",
-        duration + "B"];
-    var note = notes[keyIndex % KEYS_PER_OCTAVE];
-    note = octaveSign + note;
-    return note;
+        duration + "B"]
+    var note = notes[keyIndex % KEYS_PER_OCTAVE]
+    note = octaveSign + note
+    return note
 }
 
 function notesSVGForPAECode(paeCode) {
-    var data = "@clef:" + "G-2" + "\n";
-    data += "@keysig:" + " " + "\n";
-    data += "@timesig:" + " " + "\n";
-    console.log("notes " + paeCode);
-    data += "@data:" + paeCode;
+    var data = "@clef:" + "G-2" + "\n"
+    data += "@keysig:" + " " + "\n"
+    data += "@timesig:" + " " + "\n"
+    console.log("notes " + paeCode)
+    data += "@data:" + paeCode
 
-    var windowWidth = $(window).width();
-    console.log("Window Width: " + windowWidth);
-    var scale = 50;
+    var windowWidth = $(window).width()
+    console.log("Window Width: " + windowWidth)
+    var scale = 50
     if (windowWidth < 1000) {
-        scale = 30;
+        scale = 30
     } else if (windowWidth < 500) {
-        scale = 15;
+        scale = 15
     }
 
     options = JSON.stringify({
@@ -64,5 +64,35 @@ function notesSVGForPAECode(paeCode) {
     });
 
     var notesSVG = verovioToolkit.renderData(data, options);
-    return notesSVG;
+    return notesSVG
+}
+
+
+function pianoHTML(numberOfOctaves) {
+    //back keys are seperated to fields sharp and flat; this enables specific input
+
+    var html = '<ul class="pianokeyboard">'
+
+    for (var i = 0; i < numberOfOctaves; i++) {
+        html += '\
+                <li class="whiteKey"></li>\
+                <li class="blackKeySharp">♯</li>\
+                <li class="blackKeyFlat"><br/>♭</li>\
+                <li class="whiteKey"></li>\
+                <li class="blackKeySharp"></li>\
+                <li class="blackKeyFlat"></li>\
+                <li class="whiteKey"></li>\
+                <li class="whiteKey"></li>\
+                <li class="blackKeySharp"></li>\
+                <li class="blackKeyFlat"></li>\
+                <li class="whiteKey"></li>\
+                <li class="blackKeySharp"></li>\
+                <li class="blackKeyFlat"></li>\
+                <li class="whiteKey"></li>\
+                <li class="blackKeySharp"></li>\
+                <li class="blackKeyFlat"></li>\
+                <li class="whiteKey"></li>'
+    }
+    html +=   '</ul>'
+    return html
 }
